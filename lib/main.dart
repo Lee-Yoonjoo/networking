fbc0c8211717719cc154944eff07af348319869f
Yoon Joo Lee <yoonjoolee@INV00043.fritz.box>
2022-02-08 10:49:25 +0100

    Set http Package

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:http/http.dart' as http;
 void main() {
   runApp(MaterialApp(
     home: MyHomePage(),
@@ -9,8 +9,8 @@ void main() {
 
 
 class MyHomePage extends StatefulWidget {
- // const MyHomePage({Key? key, required this.title}) : super(key: key);
- // final String title;
+  // const MyHomePage({Key? key, required this.title}) : super(key: key);
+  // final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -18,19 +18,14 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
+  List _data = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("HTTP & JSON Tutorial"),
       ),
-      body: ListView.builder(
-          itemCount: ,
-          itemBuilder: (context, index){
-
-
-      }),
-
     );
   }
 }
