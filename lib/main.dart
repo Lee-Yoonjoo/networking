baad6d79ecfff48ebe7d0bc2caea40ea9b492326
Yoon Joo Lee <yoonjoolee@INV00043.fritz.box>
2022-02-08 11:16:33 +0100

    write a log for data

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
+//JSON, UTF-8 .. data converter for dart
+import 'dart:convert';
+//logging library
+import 'dart:developer' as developer;
+
 void main() {
   runApp(const MyApp());
 }
@@ -24,6 +29,7 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+
 class _MyHomePageState extends State<MyHomePage> {
   List _data = [];
 
@@ -39,7 +45,18 @@ class _MyHomePageState extends State<MyHomePage> {
 
       if (response.statusCode == 200) {
         String jsonString = response.body;
-        print(jsonString);
+        //check data on log
+        developer.log('Check data from HTTP response $jsonString',name :'http.get' );
+
+        List photos = jsonDecode(jsonString);
+        for (int i = 0; i<photos.length; i++){
+          var photo = photos[i];
+          Photo eachPhoto = Photo(photo["id"], photo["author"]);
+
+          String authorname = eachPhoto.author;
+          developer.log('$authorname', name :'check photo data http.get' );
+        }
+
       } else {
         print('Error!');
       }
@@ -68,4 +85,20 @@ class _MyHomePageState extends State<MyHomePage> {
           }),
     );
   }
+}
+
+
+class Photo {
+/*
+  {
+  "id": "1",
+  "author": "Alejandro Escamilla",
+  "width": 5616,
+  "height": 3744,
+  "url": "https://unsplash.com/photos/LNRyGwIJr5c",
+  "download_url": "https://picsum.photos/id/1/5616/3744"
+  }*/
+  String id;
+  String author;
+  Photo(this.id, this.author);
 }
